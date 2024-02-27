a65c3059a679f72dcbb1d300e4b3f623ab429f0c
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-26 20:25:24 -0500

    Update App file extension

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from '../Nav/Nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Devin Altobello</h1>
-      <h2>Software Engineer</h2>
-      <a href="mailto:dl.altobello@gmail.com" class="email">dl.altobello@gmail.com</a>
+      <Nav />
     </>
   )
 }
