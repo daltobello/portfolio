cb41efbc9804f357db8bb08914a80ba33484f13f
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-28 16:23:19 -0500

    Add classNames for styling

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,15 +3,15 @@ import React from "react";
 
 function Header() {
   return (
-    <>
-      <div className='header-wrapper'>
-        <h1>Devin Altobello</h1>
-        <h2>Software Engineer</h2>
-        <a href='mailto:dl.altobello@gmail.com' class='email'>
+    <div className='header-wrapper'>
+      <div className='header'>
+        <h1 className='name'>Devin Altobello</h1>
+        <h2 className='title'>Software Engineer</h2>
+        <a className='email' href='mailto:dl.altobello@gmail.com' class='email'>
           dl.altobello@gmail.com
         </a>
       </div>
-    </>
+    </div>
   );
 }
 
