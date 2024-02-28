6a766f2a5eaf30e75478eab20934d24f82844c9a
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-28 16:24:13 -0500

    Remove selected works from Nav component

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -4,19 +4,10 @@ import React from "react";
 function Nav() {
   return (
     <>
-      <ul>
-        <li className="list-items">
-          <a href='https://experience-exchange.vercel.app/'>
-            Experience Exchange
-          </a>
-        </li>
-        <li className="list-items">
-          <a href='https://word-atlas.vercel.app/'>Word Atlas</a>
-        </li>
-        <li className="list-items">
-          <a href="https://stretch-tech-theta.vercel.app/">MET Gallery</a>
-        </li>
-      </ul>
+    <div className="nav-wrapper">
+
+    </div>
+      
     </>
   );
 }
