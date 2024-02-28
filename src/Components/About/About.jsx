0892f6b444e3201a0d0fb567f5e8abcc0e6a641d
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-28 17:47:13 -0500

    Add About component with NavLink

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,15 @@
+import './About.css'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function About() {
+  return (
+    <>
+      <div>
+      <NavLink to={'/'}>Home</NavLink>
+      </div>
+    </>
+  )
+}
+
+export default About
