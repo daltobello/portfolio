c15ffa505957c89f5a893bc34ef1c64e87841dc3
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-28 18:12:56 -0500

    Add useMatch hook for conditional rendering

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,14 @@
 import "./Nav.css";
 import React from "react";
+import { NavLink, useMatch } from "react-router-dom";
 
 function Nav() {
+  const aboutMatch = useMatch("/about")
   return (
-    <>
-    <div className="nav-wrapper">
-
-    </div>
-      
-    </>
+    <nav className="nav-wrapper">
+      { aboutMatch && <NavLink to={'/'}>Home</NavLink>}
+      <NavLink to={'/about'}>About</NavLink>
+    </nav>
   );
 }
 
