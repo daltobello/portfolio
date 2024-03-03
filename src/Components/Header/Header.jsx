3c6167a4593e16fd6e76a9fe2aeec29eaadfa6eb
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-03-02 20:18:27 -0500

    Add name wrapper div

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,10 @@ import React from "react";
 function Header() {
   return (
     <div className='header-wrapper'>
-      <div className='header'>
+      <div className='name-wrapper'>
         <h1 className='name'>Devin Altobello</h1>
+      </div>
+      <div className='header'>
         <h2 className='title'>Software Engineer</h2>
         <a className='email' href='mailto:dl.altobello@gmail.com'>
           dl.altobello@gmail.com
