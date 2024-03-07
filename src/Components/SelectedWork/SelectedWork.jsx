453d91e01e5eaf9bf1f4ffc7db4ce0a76b311da2
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-03-07 10:30:58 -0500

    Update SelectedWorks grid styling

--- src/Components/SelectedWork/SelectedWork.jsx
+++ src/Components/SelectedWork/SelectedWork.jsx
@@ -1,21 +1,21 @@
 import "./SelectedWork.css";
 import React from "react";
 
-function SelectedWork({works}) {
-  
+function SelectedWork({ works }) {
   const workElements = works.map((work) => {
     return (
-      <li className="works" key={work.id} >
-        <a className="works-links" href={work.url}>{work.title}</a>
+      <li className='works' key={work.id}>
+        <a className='works-links' href={work.url}>
+          {work.title}
+        </a>
       </li>
-    )
-  })
+    );
+  });
 
   return (
-    <div className="works-wrapper">
-      <ul className="works-list"> Selected Works
-        {workElements}
-      </ul>
+      <div className='works-wrapper'>
+        <ul className='works-list'>Selected Works {workElements}
+        </ul>
       </div>
   );
 }
